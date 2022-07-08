f16d5b5333568fd903f5f7b5064edacf5f5141cf
raphaelwho <raphael.lhs@gmail.com>
2022-07-08 10:20:23 -0400

    add boto3 to get bucket obj

--- load_dat.py
+++ load_dat.py
@@ -1,3 +1,9 @@
-def load(data):
-    print(data)
+import boto3
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('nyse.taq.prod.rawfiles')
+
+def load(path):
+    for obj in bucket.objects.filter(Prefix=path):
+        print(obj.key)
+       
     return None
